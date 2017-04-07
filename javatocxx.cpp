51a6be34b940681c9354ad7b9eee5742fee4e112
ZCXL <1341772976@qq.com>
2017-04-07 08:06:35 +0800

    remove test code

--- javatocxx.cpp
+++ javatocxx.cpp
@@ -5,7 +5,6 @@
 	> Created Time: Fri 31 Mar 2017 10:47:57 AM UTC
  ************************************************************************/
 #include <iostream>
-#include "jni_bridge/com/javatocpp/test/Person.h"
 #include "jni_bridge/java/lang/String.h"
 #include "jni_bridge/java/lang/StringBuilder.h"
 #include "jni_bridge/java/util/ArrayList.h"
@@ -33,21 +32,9 @@ int main(int argc, char *argv[]) {
 	std::cout << rep << std::endl;
 	std::cout << str.toString() << std::endl;
 	java::StringBuilder builder;
-	//builder.append(java::bridge_string("hello, "));
 	builder.append("hello, ");
 	builder.append("world!");
 	std::cout << builder.toString() << std::endl;
 	std::cout << String::valueOf(100) << std::endl;
-	std::cout << Person::get_staticString() << std::endl;
-	java::Person person;
-	java::Person::InnerClass inner_class(person);
-	std::cout << inner_class.getA() << std::endl;
-	std::cout << inner_class.getB() << std::endl;
-	java::ArrayList list;
-	for (int i = 0; i < 10; i++) {
-		java::Person tmp;
-		list.add(tmp);
-	}
-	std::cout << list.size() << std::endl;
 	return 0;
 }
